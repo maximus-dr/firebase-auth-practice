f7a10dde559b99776acfe86a858525112439cd9a
Max Ivanov <maximus-dr@mail.ru>
2021-01-21 11:11:29 +0300

    Created SignUp component with styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import SignUp from "./SignUp";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <SignUp />
+      </div>
+    </Container>
   );
 }
 
