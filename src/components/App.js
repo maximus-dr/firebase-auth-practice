b5a4ca8d9a8737f0efc42d78a8ff0b93e9705e10
Max Ivanov <maximus-dr@mail.ru>
2021-01-21 16:00:43 +0300

    Added Login component and routing between Login page and Sign Up page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import SignUp from "./SignUp";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import AuthProvider from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
 
 
 function App() {
@@ -9,7 +11,14 @@ function App() {
     <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <SignUp />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
     </AuthProvider>
